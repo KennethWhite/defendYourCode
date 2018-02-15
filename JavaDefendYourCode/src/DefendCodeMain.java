99e115deeccabcc1e556011c097db3bb1a23554b
Kenneth White <kennival@hotmail.com>
2018-02-15 11:05:54 -0800

    Refactored the password functions into their own PasswordUtil class. Changed the PW regex to allow some special characters. Moved PW regex to PasswordUtil class Added an output file check, if the file exists it must also be write-able. Refactored the way we get and verify passwords to be more modular.

--- JavaDefendYourCode/src/DefendCodeMain.java
+++ JavaDefendYourCode/src/DefendCodeMain.java
@@ -1,7 +1,7 @@
 import logging.MyLogger;
 import utils.FileUtil;
 import utils.InputUtil;
-import Password.Password;
+import utils.PasswordUtil;
 
 import java.io.*;
 import java.util.Scanner;
@@ -25,10 +25,10 @@ public class DefendCodeMain {
             num1 = InputUtil.intInput(kb);
             num2 = InputUtil.intInput(kb);
 
-            inputFH = getFile(kb, true, "input");
-            outputFH = getFile(kb, false, "output");
+            inputFH = getFile(kb, true);
+            outputFH = getFile(kb, false);
 
-            passwordWork(kb);
+            PasswordUtil.getAndVerifyPassword(kb);
 
             fout = FileUtil.getPrintWriter(outputFH);
             fout.printf("%s %s\n", fName, lName);
@@ -66,10 +66,12 @@ public class DefendCodeMain {
         return name;
     }
 
-    //should we restrict maximum filesize?
-    private static File getFile(Scanner kb, boolean isInput, String fileType){
+
+    private static File getFile(Scanner kb, boolean isInput){
+        String fileType = (isInput) ? "input" : "output";
         String display = "Please enter name of the " + fileType + " file(must be in local directory): ";
         String fileName = InputUtil.stringInput(kb, display);
+
         boolean valid = (isInput) ? FileUtil.isValidInputFile(fileName) : FileUtil.isValidOutputFile(fileName);
         while(!valid){
             System.out.println("That is not a valid file name, or not in the local directory, please retry.");
@@ -78,74 +80,12 @@ public class DefendCodeMain {
             fileName = InputUtil.stringInput(kb, display);
             valid = (isInput) ? FileUtil.isValidInputFile(fileName) : FileUtil.isValidOutputFile(fileName);
         }
-
         return new File(fileName);
     }
 
-    //TODO: organize (i was just testing things so I didnt make it pretty)
-    private static void passwordWork(Scanner kb){
-
-        //TODO: finish
-
-        //get salt
-        byte[] salt = Password.getSalt();
-
-        //get password
-        String pw = InputUtil.stringInput(kb, "Please enter a password: ");
-        while(!InputUtil.passwordPassesRegex(pw)){
-            System.out.println("That is not a valid password." +
-                    "\nMust be ________");//TODO
-            MyLogger.log(Level.WARNING, "Invalid password entered: \n\tPassword:{0}", pw);
-
-            pw = InputUtil.stringInput(kb, "Please enter a password: ");
-        }
-
-        //get hash
-        byte[] hashedPW = Password.hash(pw.toCharArray(), salt);
-
-        //store hash
-        try{
-            FileOutputStream out = new FileOutputStream(new File("password"));
-
-            out.write(hashedPW);
-
-            out.close();
-
-        }
-        catch(Exception e){
-            //TODO: log and handle
-        }
 
 
-        //get password again
-        String pwAgain = InputUtil.stringInput(kb, "Please enter the password again: ");
-        while(!InputUtil.passwordPassesRegex(pwAgain)){
-            System.out.println("That is not a valid password." +
-                    "\nMust be ________");//TODO
-            MyLogger.log(Level.WARNING, "Invalid password entered: \n\tPassword:{0}", pwAgain);
 
-            pwAgain = InputUtil.stringInput(kb, "Please enter the password again: ");
-        }
-
-        //retrieve hash
-        byte[] existingHash = new byte[32];
-        try{
-            FileInputStream in = new FileInputStream(new File("password"));
-
-            in.read(existingHash);
-
-            in.close();
-
-        }
-        catch(Exception e){
-            //TODO: log and handle
-        }
-
-        //compare hash
-        boolean samePassword = Password.samePassword(pwAgain.toCharArray(), salt, existingHash);
-
-        System.out.println(samePassword);
-    }
 
     private static long addInts(int num1, int num2){
         long res = 0;
